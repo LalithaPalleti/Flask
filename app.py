33279b9083bc5fc4b27037883ff7da47b914c44a
LalithaPalleti <31676766+LalithaPalleti@users.noreply.github.com>
2018-03-27 08:01:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request, redirect
-
+import json
+import requests
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+import quandl
+quandl.ApiConfig.api_key = 'hMy3FpyQh61czyanLwbC'
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,5 +16,16 @@ def index():
 def about():
   return render_template('about.html')
 
+@app.route('/plot',methods = ['GET','POST'])
+def plot():
+    data = quandl.get_table('WIKI/PRICES',
+                       ticker = request.form['ticker'])
+    #output_file("./templates/line.html")
+    p = figure(plot_width=400, plot_height=400,x_axis_type = "datetime")
+    p.line(data.date,data.open,line_width = 2,legend = request.form['ticker'],color = 'green')
+    script, div = components(p)
+    return render_template('line.html',div=div,script=script)
+
+
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(host = '0.0.0.0',port=33507,debug='true')
